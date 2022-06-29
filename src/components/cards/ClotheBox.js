fa657fe0fdcb408813d8f1a4edde3defd8638eec
Anteneh Solomon <antenehcs@gmail.com>
2022-06-29 13:50:11 +0300

    Card Box

--- src/components/cards/ClotheBox.js
+++ src/components/cards/ClotheBox.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import '../styles/style.css'
+import photo from '../Images/imagetwo.jpg'
 
 const ClotheBox = () => {
   return (
-    <div>
-      <div className='clotheboxmain'>
-        <h5 className='details'>Name: </h5>
-        <h5 className='details'>Price: </h5>
-        <h5 className='details'>Gender: </h5>
-        <h5 className='details'>Age: </h5>
-        <h5 className='details'>Description: </h5>  
+    <div className='mainbox'>
+      <div className='container'>
+        <div className='card'>
+        <img src={photo} className="pic"/>
+        </div>
+        <div className='intro'>
+          <h5 className='details'>Name</h5>
+          <div className='but'>
+            <h5 className='detailsbut'>View</h5>
+          </div>
+        </div>
       </div>
     </div>
   )
