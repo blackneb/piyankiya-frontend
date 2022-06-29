8f44ac66402e243e9cebb6c326620a4e8660887c
Anteneh Solomon <antenehcs@gmail.com>
2022-06-29 15:04:30 +0300

    About

--- src/components/body/about/About.js
+++ src/components/body/about/About.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import '../../styles/style.css'
+import Photo from '../../Images/imageone.jpg'
 
 const About = () => {
   return (
     <div>
-      
+      <div>
+        <h1 className='aboutname'>About us</h1>
+        <div className='aboutsplit'>
+        <div className='aboutimage'>
+          <img src={Photo} className='aboutpic'/>
+        </div>
+        <div className='about_inner'>
+          <p> Habesha kemis is the traditional attire of Habesha women.The ankle length dress is usually 
+            worn by Ethiopian and Eritrean women at formal events, holidays and invitations. But it comes 
+            in many forms nowadays. It is made of cotton fabric, and typically comes in white, grey or beige 
+            shades. Many women also wrap a shawl called a netela around the formal dress. </p>
+            <div className='aboutaddmain'>
+              <h5 className='aboutadd'>Address: </h5>
+              <h5 className='aboutadd'>Phone: </h5>
+              <h5 className='aboutadd'>E-mail: </h5>
+            </div>
+        </div>
+        </div>
+      </div>
     </div>
   )
 }
