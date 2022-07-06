8ea4366b8c0f5abeb31657215353361c16f63a60
Anteneh Solomon <antenehcs@gmail.com>
2022-07-06 15:26:13 +0300

    details form validation

--- src/components/body/detailed/Detailed.js
+++ src/components/body/detailed/Detailed.js
@@ -1,8 +1,69 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Photo from '../../Images/imageone.jpg'
 import '../../styles/style.css'
+import FormInput from '../../Forms/FormInput'
 
 const Detailed = () => {
+  const [values, setValues] = useState({
+    name: "",
+    phone: "",
+    email: "",
+    quantity: "",
+  });
+
+
+  const inputs = [
+    {
+      id: 1,
+      name: "name",
+      type: "text",
+      placeholder: "Name",
+      errorMessage: "name should be 3-16 characters and shouldn't include any special character!",
+      label: "Name",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "phone",
+      type: "text",
+      placeholder: "Phone Number",
+      errorMessage: "Please enter the field",
+      label: "phone",
+      required: true,
+    },
+    {
+      id: 3,
+      name: "email",
+      type: "email",
+      placeholder: "E-mail",
+      errorMessage:"Please Enter the correct email",
+      label: "E-mail",
+      required: true,
+    },
+    {
+      id: 4,
+      name: "quantity",
+      type: "number",
+      placeholder: "Quantity",
+      errorMessage: "How much items do you want",
+      label: "Quantity",
+      required: true,
+    },
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(JSON.stringify(values, null,2))
+  };
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+
+
+
   return (
     <div>
         <div className='detailedmain'>
@@ -18,17 +79,17 @@ const Detailed = () => {
             <h3 className='conlabel'>Type: </h3>
           </div>
           <div className='detailedthird'>
-            <form>
-              <label className='conlabel'>Name</label><br/>
-              <input className='coninput'/><br/>
-              <label className='conlabel'>E-mail</label><br/>
-              <input className='coninput'/><br/>
-              <label className='conlabel'>Phone</label><br/>
-              <input className='coninput'/><br/>
-              <label className='conlabel'>Quantity</label><br/>
-              <input className='coninput'/><br/>
-              <input className='submail' type="submit" value="Book"></input>
-            </form>
+          <form onSubmit={handleSubmit}>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+        <input className='submail' type="submit" value="Book"></input>
+      </form>
           </div>
 
 
