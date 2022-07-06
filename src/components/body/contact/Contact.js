3cddaef6a63dd9c9e619e9545f540f4f5b7d9991
Anteneh Solomon <antenehcs@gmail.com>
2022-07-06 15:19:49 +0300

    Contact validation

--- src/components/body/contact/Contact.js
+++ src/components/body/contact/Contact.js
@@ -1,27 +1,94 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../../styles/style.css'
+import FormInput from '../../Forms/FormInput'
 
 const Contact = () => {
+  const [values, setValues] = useState({
+    name: "",
+    phone: "",
+    subject: "",
+    email: "",
+    message: "",
+  });
+
+
+
+  const inputs = [
+    {
+      id: 1,
+      name: "name",
+      type: "text",
+      placeholder: "Name",
+      errorMessage: "name should be 3-16 characters and shouldn't include any special character!",
+      label: "Name",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "phone",
+      type: "text",
+      placeholder: "Phone Number",
+      errorMessage: "Please enter the field",
+      label: "phone",
+      required: true,
+    },
+    {
+      id: 3,
+      name: "subject",
+      type: "text",
+      placeholder: "Subject",
+      label: "Subject",
+      required: true,
+    },
+    {
+      id: 4,
+      name: "email",
+      type: "email",
+      placeholder: "E-mail",
+      errorMessage:"Please Enter the correct email",
+      label: "E-mail",
+      required: true,
+    },
+    {
+      id: 5,
+      name: "message",
+      type: "textarea",
+      placeholder: "Message",
+      errorMessage: "Please Enter thr price",
+      label: "Message",
+      required: true,
+    },
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(JSON.stringify(values, null,2))
+  };
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+
+
+
   return (
     <div>
       <div className='main_contact'>
         <h1>Contact us</h1>
         <div className='contactmail'>
-                  <form>
-                      <label className='conlabel'>Name</label> <br/>
-                      <input className='coninput' type="text" name="name" /> <br/>
-                      <label className='conlabel'>Phone number</label> <br/>
-                      <input className='coninput' type="text" name="phone"/> <br/>
-                      <label className='conlabel'>Subject</label> <br/>
-                      <input className='coninput' type="text" name="subject" /> <br/>
-                      <label className='conlabel'>Email</label> <br/>
-                      <input className='coninput' type="email" name="email" /> <br/>
-                      <label className='conlabel'>Message</label>
-                      <br/>
-                      <textarea className='coninput' name='message' ></textarea>
-                      <br/>
-                      <input className='submail' type="submit" value="Send"></input>
-                  </form>
+        <form onSubmit={handleSubmit}>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+        <input className='submail' type="submit" value="Send"></input>
+      </form>
               </div>
       </div>
     </div>
