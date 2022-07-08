97bf5d762001c6d35e35929cac839434d7c0189f
Anteneh Solomon <antenehcs@gmail.com>
2022-07-08 20:10:15 +0300

    update

--- src/components/body/men/Men.js
+++ src/components/body/men/Men.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Break from '../break/Break'
 import ClotheBox from '../../cards/ClotheBox'
 import '../../styles/style.css'
 import axios from "axios";
 
 const Men = () => {
-  const baseURL = "http://localhost/piyankiya/api/post/read.php";
-  const [post, setPost] = React.useState(null);
+  const baseURL = "http://localhost/piyankiya/api/post/read_byg.php?gender=male";
+  const [post, setPost] = React.useState("");
   React.useEffect(() => {
-    axios.post(baseURL).then((response) => {
+    axios.get(baseURL).then((response) => {
       setPost(response.data);
     });
   }, []);
@@ -20,8 +20,8 @@ const Men = () => {
       <h1>Men Habeshan Clothes</h1>
       <div className='homewo'>
         <div className='homewoboxes'>
-          {post.data.map((e) => (
-            <ClotheBox name={e.name}/>
+        {post.data.map((e) => (
+            <ClotheBox name={e.name} id={e.id} price={e.price} description={e.description} photo={e.photo}/>
           ))}
         </div>
       </div>
