02e508d1476a24a77f75d96e794229b11f2c95b9
Anteneh Solomon <antenehcs@gmail.com>
2022-07-08 20:13:15 +0300

    kids update

--- src/components/body/kids/Kids.js
+++ src/components/body/kids/Kids.js
@@ -2,26 +2,27 @@ import React from 'react'
 import Break from '../break/Break'
 import ClotheBox from '../../cards/ClotheBox'
 import '../../styles/style.css'
+import axios from "axios";
 
 const Kids = () => {
+  const baseURL = "http://localhost/piyankiya/api/post/read_bya.php?age=kids";
+  const [post, setPost] = React.useState("");
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  if (!post) return null;
   return (
     <div>
       <Break/>
       <h1>Kids Habeshan Clothes</h1>
       <div className='homewo'>
         <div className='homewoboxes'>
-          <ClotheBox/>
-          <ClotheBox/>
-          <ClotheBox/>
-          <ClotheBox/>
-          <ClotheBox/>
-          <ClotheBox/>
-          <ClotheBox/>
-          <ClotheBox/>
-          <ClotheBox/>
-          <ClotheBox/>
-          <ClotheBox/>
-          <ClotheBox/>
+        {post.data.map((e) => (
+            <ClotheBox name={e.name} id={e.id} price={e.price} description={e.description} photo={e.photo}/>
+          ))}
         </div>
       </div>
       <Break/>
