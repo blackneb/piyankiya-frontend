575c4f4dd997ae63f32dfdef5914126fe6e77524
Anteneh Solomon <antenehcs@gmail.com>
2022-07-24 13:16:28 +0300

    home update

--- src/components/body/home/Home.js
+++ src/components/body/home/Home.js
@@ -8,22 +8,17 @@ import About from '../about/About'
 
 
 const Home = () => {
-  const baseURL = "http://localhost/piyankiya/api/post/read.php";
-  const baseURLMEN = "http://localhost/piyankiya/api/post/read_byg.php?gender=male";
-  const baseURLWOMEN = "http://localhost/piyankiya/api/post/read_byg.php?gender=female";
-  const baseURLKIDS = "http://localhost/piyankiya/api/post/read_bya.php?age=kids";
-  const baseURLOCCASIONS = "http://localhost/piyankiya/api/post/read_byt.php?types=occasion";
+  const baseURLMEN = "http://blackneb.com/piyankiya/api/post/read_byg.php?gender=male";
+  const baseURLWOMEN = "http://blackneb.com/piyankiya/api/post/read_byg.php?gender=female";
+  const baseURLKIDS = "http://blackneb.com/piyankiya/api/post/read_bya.php?age=kids";
+  const baseURLOCCASIONS = "http://blackneb.com/piyankiya/api/post/read_byt.php?types=occasion";
   const [post, setPost] = React.useState(null);
   const [men,setmen]= React.useState(null);
   const [women,setwomen]= React.useState(null);
   const [kids,setkids]= React.useState(null);
   const [occasion,setoccasions]= React.useState(null);
 
-
   React.useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setPost(response.data);
-    });
     axios.get(baseURLMEN).then((response) => {
       setmen(response.data);
     });
@@ -38,7 +33,10 @@ const Home = () => {
     });
   }, []);
 
-  if (!post) return null;
+  if (!men) return null;
+  if (!women) return null;
+  if (!kids) return null;
+  if (!occasion) return null;
   return (
     <div>
       <Break/>
@@ -47,17 +45,39 @@ const Home = () => {
         <h1>For Women</h1>
         </div>
         <div className='homewoboxes'>
-          {women.data.map((e) => (
-            <ClotheBox name={e.name} id={e.id} price={e.price} description={e.description} photo={e.photo}/>
-          ))}
+          {(()=>{
+                    if(women.message==="no posts found"){
+                        return(
+                            <h1>we will add contents soon</h1>
+                        )
+                    }
+                    else{
+                        return(
+                          women.data.slice(0,3).map((e) => (
+                            <ClotheBox name={e.name} id={e.id} price={e.price} description={e.description} photo={e.photos} age={e.afor} gender={e.gfor} types={e.types}/>          
+                            ))
+                        )
+                    }
+                })()}
         </div>
       </div>
       <Break/>
       <div className='homewo'>
       <div className='homewoboxes'>
-        {men.data.map((e) => (
-              <ClotheBox name={e.name} id={e.id} price={e.price} description={e.description} photo={e.photo}/>
-            ))}
+      {(()=>{
+                    if(men.message==="no posts found"){
+                        return(
+                            <h1>we will add contents soon</h1>
+                        )
+                    }
+                    else{
+                        return(
+                          men.data.slice(0,3).map((e) => (
+                            <ClotheBox name={e.name} id={e.id} price={e.price} description={e.description} photo={e.photos} age={e.afor} gender={e.gfor} types={e.types}/>          
+                            ))
+                        )
+                    }
+                })()}
         </div>
         <div className='homewonname'>
         <h1>For Men</h1>
@@ -69,17 +89,39 @@ const Home = () => {
         <h1>For Occasions</h1>
         </div>
         <div className='homewoboxes'>
-          {occasion.data.map((e) => (
-              <ClotheBox name={e.name} id={e.id} price={e.price} description={e.description} photo={e.photo}/>
-            ))}
+        {(()=>{
+                    if(occasion.message==="no posts found"){
+                        return(
+                            <h1>we will add contents soon</h1>
+                        )
+                    }
+                    else{
+                        return(
+                          occasion.data.slice(0,3).map((e) => (
+                            <ClotheBox name={e.name} id={e.id} price={e.price} description={e.description} photo={e.photos} age={e.afor} gender={e.gfor} types={e.types}/>          
+                            ))
+                        )
+                    }
+                })()}
         </div>
       </div>
       <Break/>
       <div className='homewo'>
       <div className='homewoboxes'>
-        {kids.data.map((e) => (
-              <ClotheBox name={e.name} id={e.id} price={e.price} description={e.description} photo={e.photo}/>
-            ))}
+      {(()=>{
+                    if(kids.message==="no posts found"){
+                        return(
+                            <h1>we will add contents soon</h1>
+                        )
+                    }
+                    else{
+                        return(
+                          kids.data.slice(0,3).map((e) => (
+                            <ClotheBox name={e.name} id={e.id} price={e.price} description={e.description} photo={e.photos} age={e.afor} gender={e.gfor} types={e.types}/>          
+                            ))
+                        )
+                    }
+                })()}
         </div>
         <div className='homewonname'>
         <h1>For Kids</h1>
