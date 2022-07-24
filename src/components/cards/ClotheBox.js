31c5ba44653459bb67843664b6186711f0b93204
Anteneh Solomon <antenehcs@gmail.com>
2022-07-24 13:28:22 +0300

    card box updated

--- src/components/cards/ClotheBox.js
+++ src/components/cards/ClotheBox.js
@@ -9,16 +9,20 @@ const ClotheBox = (props) => {
   const [description,setdescription] = useState(props.description);
   const [photos,setphotos] = useState(props.photo);
   const [id,setid] = useState(props.id);
+  const [age,setage]=useState(props.age);
+  const [gender,setgender]=useState(props.gender);
+  const [types,settypes]=useState(props.types);
+
   return (
     <div className='mainbox'>
       <div className='container'>
         <div className='card'>
-        <img src={photo} alt='' className="pic"/>
+        <img src={photos} alt='' className="pic"/>
         </div>
         <div className='intro'>
           <h5 className='details'>{props.name}</h5>
           <div className='but'>
-            <Link  to="/detailed"  state={{fname:name,fphoto:photos,fprice:price,fdescription:description, fid:id }} className='detailsbut'>
+            <Link  to="/detailed"  state={{fname:name,fphoto:photos,fprice:price,fdescription:description, fid:id, fage:age, fgender:gender, ftypes:types }} className='detailsbut'>
               <li className='linkdetails'><h5 className='detailsbut'>View</h5></li>
             </Link>
           </div>
