de1133cc4f2f8aac272e7dbca1924741902be8ed
Anteneh Solomon <antenehcs@gmail.com>
2022-08-30 13:29:26 +0300

    women update

--- src/components/body/women/Women.js
+++ src/components/body/women/Women.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Break from '../break/Break'
 import ClotheBox from '../../cards/ClotheBox'
 import '../../styles/style.css'
 import axios from "axios";
+import { ActionTypes } from '../../../redux/Constants/ActionTypes';
+import { set_clothes } from '../../../redux/Actions/Actions';
+import {useSelector, useDispatch} from 'react-redux';
+import loadingpage from '../../icons/loadingpage.gif';
 
 const Women = () => {
-  const baseURL = "http://blackneb.com/piyankiya/api/post/read_byg.php?gender=female";
-  const [post, setPost] = React.useState("");
-  React.useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setPost(response.data);
-    });
-  }, []);
-
-  if (!post) return null;
+  const [loading, setloading] = useState(false);
+  const dispatch = useDispatch();
+  const clothes = useSelector(state => state.clothes.clothes);
+  let womenclothe = clothes.filter(clothe => clothe.gfor === 'female');
+  if(clothes.length === 0 ){
+    axios.get(ActionTypes.BASEURL + "/read.php").then((response) => {
+      dispatch(set_clothes(response.data.data));
+      setloading(true);
+    })
+  }
   return (
     <div>
       <Break/>
@@ -21,15 +26,20 @@ const Women = () => {
       <div className='homewo'>
         <div className='homewoboxes'>
         {(()=>{
-                    if(post.message==="no posts found"){
+                    if(clothes.length===0){
                         return(
-                            <h1>we will add contents soon</h1>
-                        )
+                          <h1><img className='loadingpage' src={loadingpage}  alt="loadingpage"/></h1>
+                          )
+                    }
+                    else if(womenclothe.length===0){
+                      return(
+                        <h1>we will add contents soon</h1>
+                      )
                     }
                     else{
                         return(
-                          [...post.data].reverse().map((e) => (
-                            <ClotheBox key={e.id} name={e.name} id={e.id} price={e.price} description={e.description} photo={e.photos} age={e.afor} gender={e.gfor} types={e.types}/>          
+                          [...womenclothe].reverse().map((e) => (
+                            <ClotheBox key={e.id} name={e.name} id={e.id} price={e.price} description={e.description} photo={ActionTypes.PHOTOURL + e.photos} age={e.afor} gender={e.gfor} types={e.types}/>          
                             ))
                         )
                     }
